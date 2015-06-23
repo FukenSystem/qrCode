c6f630828a2dace99b541cf9ede436173cb54c98
FukenSytem <yaremchukvk@gmail.com>
2015-06-24 00:10:36 +0300

    Initial Commit

---
+++ qrCodeTests/qrCodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  qrCodeTests.swift
+//  qrCodeTests
+//
+//  Created by blackice on 6/24/15.
+//  Copyright (c) 2015 WebAcademyTeam. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class qrCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
